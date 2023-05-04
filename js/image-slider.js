74d65f331471c0ea34dd4f91ec3cab2285c4eb73
David Colvin <davidmcolvin@gmail.com>
2023-05-03 20:47:44 -0400

    Add more changes to make it look better

---
+++ js/image-slider.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+	let slideIndex = 1;
+	showSlides(slideIndex);
+
+	$('.prev-btn').click(function() {
+		showSlides(slideIndex -= 1);
+	});
+	
+	$('.next-btn').click(function() {
+		showSlides(slideIndex += 1);
+	});
+
+	function showSlides(n) {
+		let i;
+		let slides = document.getElementsByClassName("slide");
+		
+		if (n > slides.length) {
+			slideIndex = 1;
+		}
+		if (n < 1) {
+			slideIndex = slides.length;
+		}
+		for (i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none";
+		}
+		slides[slideIndex - 1].style.display = "block";
+	}
+});
